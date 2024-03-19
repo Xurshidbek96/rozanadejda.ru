a4dc24786e31a6055c77c4fdffec9f855f6d954d
Xurshidbek <mominovxurshidbek66@gmail.com>
2024-03-19 15:52:12 +0500

    bug fix

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::latest()->get();
+        $products = ProductResource::collection(Product::latest()->get());
         return $this->checkData($products);
     }
 
