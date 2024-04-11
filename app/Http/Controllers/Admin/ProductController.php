4d9a9bf5a9533b02812c31695f5cdbd00820ab9e
Xurshidbek <mominovxurshidbek66@gmail.com>
2024-04-11 18:08:37 +0500

    update

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,92 +6,42 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductStoreRequest;
 use App\Http\Requests\ProductUpdateRequest;
 use App\Http\Resources\ProductResource;
-use App\Models\Image;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Services\ProductAdminService;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
+    public $productAdminService;
+    public function __construct(ProductAdminService $productAdminService)
+    {
+        $this->productAdminService = $productAdminService;
+    }
+
     public function index()
     {
-        $products = ProductResource::collection(Product::latest()->paginate(10));
-        return $products;
+        return $this->productAdminService->getAllProducts();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(ProductStoreRequest $request)
     {
-        $requestData = $request->except('files');
-        $product = Product::create($requestData);
-        if ($request->hasFile('files')) {
-            $files = $request->file('files');
-            foreach ($files as $file) {
-                $filename = time() . '-' . $file->getClientOriginalName();
-                $file->move(public_path('images/products'), $filename);
-                $images[] = Image::create([
-                    'product_id' => $product->id,
-                    'filename' => $filename
-                ]);
-            }
-        } else $images = null;
-        return $this->checkData($product);
+        $data = $this->productAdminService->store($request);
+        return $this->checkData($data);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Product $product)
     {
         $data =  new ProductResource($product);
         return $this->checkData($data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function productUpdate(ProductUpdateRequest $request, Product $product)
     {
-        // return $request ;
-        $requestData = $request->except('files');
-        $product->update($requestData);
-
-        // Rasmlarni yangilash
-        if ($request->hasFile('files')) {
-            $files = $request->file('files');
-            $images = [];
-            // Eskilarni o'chirish va ma'lumotlar bazasidan o'chirish
-            $product->images->each(function ($image) {
-                $imagePath = public_path('images/products') . '/' . $image->filename;
-                if (file_exists($imagePath)) {
-                    unlink($imagePath);
-                }
-                $image->delete();
-            });
-
-            // Yangi rasmlarni qo'shish
-            foreach ($files as $file) {
-                $filename = time() . '-' . $file->getClientOriginalName();
-                $file->move(public_path('images/products'), $filename);
-
-                $images[] = Image::create([
-                    'product_id' => $product->id,
-                    'filename' => $filename
-                ]);
-            }
-        }
-        // Ma'lumotlarni yangilangan holatda o'qish
-        $product->load('images');
-        return $this->checkData($product);
+        $data = $this->productAdminService->update($request, $product);
+        return $this->checkData($data);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Product $product)
     {
         foreach ($product->images as $image) {
---
+++ app/Http/Services/ProductAdminService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Image;
+use App\Models\Product;
+
+class ProductAdminService
+{
+    public function getAllProducts(){
+        $products = ProductResource::collection(Product::latest()->paginate(10));
+
+        return $products;
+    }
+
+    public function store($request){
+
+        $requestData = $request->except('files');
+        $product = Product::create($requestData);
+        if ($request->hasFile('files')) {
+            $files = $request->file('files');
+            foreach ($files as $file) {
+                $filename = time() . '-' . $file->getClientOriginalName();
+                $file->move(public_path('images/products'), $filename);
+                $images[] = Image::create([
+                    'product_id' => $product->id,
+                    'filename' => $filename
+                ]);
+            }
+        }
+        else $images = null;
+
+        return $product ;
+    }
+
+    public function update($request, $product){
+        // return $request ;
+        $requestData = $request->except('files');
+        $product->update($requestData);
+
+        // Rasmlarni yangilash
+        if ($request->hasFile('files')) {
+            $files = $request->file('files');
+            $images = [];
+            // Eskilarni o'chirish va ma'lumotlar bazasidan o'chirish
+            $product->images->each(function ($image) {
+                $imagePath = public_path('images/products') . '/' . $image->filename;
+                if (file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+                $image->delete();
+            });
+
+            // Yangi rasmlarni qo'shish
+            foreach ($files as $file) {
+                $filename = time() . '-' . $file->getClientOriginalName();
+                $file->move(public_path('images/products'), $filename);
+
+                $images[] = Image::create([
+                    'product_id' => $product->id,
+                    'filename' => $filename
+                ]);
+            }
+        }
+        $product->load('images');
+        return $product;
+    }
+
+}
+
